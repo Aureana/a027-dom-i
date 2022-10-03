f9e2c1a6b2356fde07f72b9eb178c278681db101
Aureana <111321782+Aureana@users.noreply.github.com>
2022-10-03 16:14:05 -0300

    ammalA-Aureana-Santos-a027

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const frutas = ["laranja", "limão", "uva"];
+let frutaLaranja = frutas[0]
+let frutaLimao = frutas[1]
+let frutaUva = frutas[2]
+
+document.getElementById("fruta-1").innerHTML = `${frutaLaranja}`
+document.getElementById("fruta-2").innerHTML = `${frutaLimao}`
+document.getElementById("fruta-3").innerHTML = `${frutaUva}`
+
+
+let imprimeValor = ()=>{console.log(document.getElementById("inputFruta4").value)}
+
+let adicioneFruta4 =() =>{
+    document.getElementById("fruta-4").innerHTML= document.getElementById("inputFruta4").value
+}
